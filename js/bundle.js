ce15a6a62a860cd06467c00ed97f0a2366cefc9e
Sadless74 <Sadless74@users.noreply.github.com>
2018-01-04 00:20:22 +0500

    Update bundle.js
    changes in URLs for overpass augmented diff

--- js/bundle.js
+++ js/bundle.js
@@ -3452,7 +3452,7 @@ var osmStream = (function osmMinutely() {
 
     // presets
     var baseUrl = 'http://overpass-api.de/',
-        minuteStatePath = 'augmented_diffs/state.txt',
+        minuteStatePath = 'api/augmented_diff_status',
         changePath = 'api/augmented_diff?';
 
     function minuteStateUrl() {
